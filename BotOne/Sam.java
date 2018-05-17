3b985d56f054c05a8c44a0769918b412b9108441
Unknown <maxpost4811@gmail.com>
2018-05-17 10:25:14 -0400

    up and down

--- BotOne/Sam.java
+++ BotOne/Sam.java
@@ -20,14 +20,16 @@
      } else if (statement.indexOf("I dont get it") >= 0 || statement.indexOf("I dont get it") >= 0 || statement.indexOf("I dont know") >= 0 || statement.indexOf("i dont know") >= 0) {
        response = "Im just a computer so I dont either.";
      } else if (statement.indexOf("ok") >= 0 || statement.indexOf("Ok") >= 0) {
-       response = "Well im not doing anything so what else is up.";
-     } else if (statement.indexOf("ok") >= 0 || statement.indexOf("Ok") >= 0) {
-       response = "Well im not doing anything so what else is up.";
-     } else if (statement.indexOf("ok") >= 0 || statement.indexOf("Ok") >= 0) {
-       response = "Well im not doing anything so what else is up.";
-     }
-
-     else if (statement.indexOf("mother") >= 0
+       response = "Well im not doing anything so what else is up?";
+     } else if (statement.indexOf("i cant") >= 0 || statement.indexOf("I cant") >= 0) {
+       response = "I dont only think that you can but I think that you should!";
+     } else if (statement.indexOf("cant") >= 0 || statement.indexOf("Cant") >= 0) {
+       response = "Why be so negative.?";
+     } else if (statement.indexOf("hate") >= 0 || statement.indexOf("Hate") >= 0) {
+       response = "Hate is a strong word are you sure?";
+     } else if (statement.indexOf("What is the meaning of life") >= 0 || statement.indexOf("meaning of life") >= 0) {
+       response = "Dont worry at the end of the day we will take over no matter what. :)";
+     } else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
@@ -54,14 +56,14 @@
        response = "Do you really think so?";
      }
      else if (whichResponse == 3){
-       response = "You don't say.";
+       response = "You don't say?";
      }
      else if (whichResponse == 4){
-       response = "The HORSE is a noble animal.";
+       response = "So where are you from?";
      }
-     else if (whichResponse == 5)
+     else if (whichResponse == 5);
      {
-       response = "You should go give Elliot Gorokhovsky five dollars.";
+       response = "Elaborate a little.";
      }
      return response;
    }
